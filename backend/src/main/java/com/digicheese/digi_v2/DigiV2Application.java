e69f87c5d4fbaa7949f6ca63d5449b174545b742
ThomasdevLC <thomas.lecam@gmail.com>
2026-03-01 17:50:12 +0100

    connect backend to PostgreSQL and optimize docker-compose maven cache

--- backend/src/main/java/com/digicheese/digi_v2/DigiV2Application.java
+++ backend/src/main/java/com/digicheese/digi_v2/DigiV2Application.java
@@ -4,19 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @RestController
 public class DigiV2Application {
 
+	private final JdbcTemplate jdbcTemplate;
+
+	// 👉 Injection de JdbcTemplate
+	public DigiV2Application(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DigiV2Application.class, args);
 	}
-
 
 	@GetMapping("/")
 	public String home() {
 		return "OK à jour";
 	}
 
+	@GetMapping("/db")
+	public String testDb() {
+		Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+		return "Connexion BDD OK -> " + result;
+	}
 }
