7c9f5da47f87113f3ec030af6e2788345089febf
ThomasdevLC <thomas.lecam@gmail.com>
2026-03-07 17:00:17 +0100

    clean folders and DigiV2Application

--- backend/src/main/java/com/digicheese/digi_v2/DigiV2Application.java
+++ backend/src/main/java/com/digicheese/digi_v2/DigiV2Application.java
@@ -4,18 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
 @RestController
 public class DigiV2Application {
 
-	private final JdbcTemplate jdbcTemplate;
-
-	// 👉 Injection de JdbcTemplate
-	public DigiV2Application(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigiV2Application.class, args);
@@ -26,9 +20,4 @@ public class DigiV2Application {
 		return "OK à jour";
 	}
 
-	@GetMapping("/db")
-	public String testDb() {
-		Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-		return "Connexion BDD OK -> " + result;
-	}
 }
