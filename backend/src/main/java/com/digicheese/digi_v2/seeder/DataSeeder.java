fe0886d66100199be4aa78689a7dee3d16bcff9e
ThomasdevLC <thomas.lecam@gmail.com>
2026-03-22 15:21:23 +0100

    add data seeder

---
+++ backend/src/main/java/com/digicheese/digi_v2/seeder/DataSeeder.java
@@ -0,0 +1,28 @@
+package com.digicheese.digi_v2.seeder;
+
+import com.digicheese.digi_v2.models.User;
+import com.digicheese.digi_v2.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSeeder {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository) {
+        return args -> {
+
+            if (userRepository.count() == 0) {
+
+                userRepository.save(
+                        new User(null, "Red", "John", "T@1234", "john@test.com")
+                );
+
+                userRepository.save(
+                        new User(null, "Black", "Anna", "T2@abcd", "anna@test.com")
+                );
+            }
+        };
+    }
+}
