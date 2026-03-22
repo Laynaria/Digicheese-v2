94ba4add700882df20535076d0dcc84de1e63350
ThomasdevLC <thomas.lecam@gmail.com>
2026-03-22 15:18:41 +0100

    delete product files example

--- backend/src/main/java/com/digicheese/digi_v2/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.digicheese.digi_v2.repositories;
-
-import com.digicheese.digi_v2.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
