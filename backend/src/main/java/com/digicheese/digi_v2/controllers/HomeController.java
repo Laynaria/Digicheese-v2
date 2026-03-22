5d5fec71e8aad9e1378bb95c9fe41487c90102ef
ThomasdevLC <thomas.lecam@gmail.com>
2026-03-22 15:19:50 +0100

    add home controller example

---
+++ backend/src/main/java/com/digicheese/digi_v2/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.digicheese.digi_v2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "test OK 2";
+    }
+}
