9fedd126fea56da9cd6cca7df7b1ed003e7862aa
ThomasdevLC <thomas.lecam@gmail.com>
2026-04-05 16:42:13 +0200

    feat: setup user entity and repository

--- backend/src/main/java/com/digicheese/digi_v2/models/User.java
+++ backend/src/main/java/com/digicheese/digi_v2/models/User.java
@@ -1,10 +1,16 @@
 package com.digicheese.digi_v2.models;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +24,7 @@ public class User {
 
     private String email;
 
+
     public User() {
     }
 
@@ -27,6 +34,31 @@ public class User {
         this.firstname = firstname;
         this.password = password;
         this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public Integer getId() {
@@ -55,6 +87,11 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
     }
 
     public void setPassword(String password) {
