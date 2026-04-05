5ecac152ff4dcef571355d63ca16b7436b3541a0
ThomasdevLC <thomas.lecam@gmail.com>
2026-04-05 16:42:58 +0200

    feat: add jwt config and service

---
+++ backend/src/main/java/com/digicheese/digi_v2/services/JwtService.java
@@ -0,0 +1,30 @@
+package com.digicheese.digi_v2.services;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("{$security.jwt.expiration-time}")
+    private long expirationTime;
+
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+
+    }
+
+
+
+}
